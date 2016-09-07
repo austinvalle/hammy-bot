057858190ae0165726d1dd452d74f39132e44442
Austin <austinvalle@gmail.com>
2016-09-07 00:33:30 -0400

    added mp4 to gif conversion with fluent-ffmpeg

---
+++ src/modules/videos/videos.js
@@ -0,0 +1,43 @@
+var Q = require('q');
+
+var images = require('../images/images');
+var cache = require('../shared/cache/cache');
+var gif = require('../shared/gif/gif');
+
+module.exports = {
+    upload_from_url: function(url, filename, uniqueId, videoLength) {
+        var deferred = Q.defer();
+
+        var videoParams = determineVideoParameters(videoLength);
+
+        cache.download(url, filename, function(downloadPath) {
+            gif.convert_mp4(downloadPath, uniqueId, videoParams.start, videoParams.duration).then(function(gifPath) {
+                images.upload_from_path(gifPath, uniqueId + '.gif')
+                    .then(function(id) {
+                        deferred.resolve(id);
+                    });
+            });
+        });
+
+        return deferred.promise;
+    }
+};
+
+var determineVideoParameters = function(videoLength) {
+    if (videoLength <= 10) {
+        return {
+            start: 0,
+            duration: videoLength
+        }
+    } else if (videoLength <= 15) {
+        return {
+            start: videoLength - 10,
+            duration: 10
+        }
+    } else {
+        return {
+            start: 5,
+            duration: 10
+        }
+    }
+}
