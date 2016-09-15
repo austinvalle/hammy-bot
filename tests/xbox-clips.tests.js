69235259a60bef369656d96e3a175b5fba062124
Austin <austinvalle@gmail.com>
2016-09-15 01:13:48 -0400

    added tests for xboxdvr game clips

---
+++ tests/xbox-clips.tests.js
@@ -0,0 +1,56 @@
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var rewire = require("rewire");
+
+var Q = require('q');
+var videos = require('../lib/modules/media/videos');
+
+var xboxClips = rewire('../lib/modules/xbox/xbox-clips');
+
+describe('xbox-clips module', function() {
+    describe('upload xboxdvr', function() {
+        var videoDeferred,
+            XrayStub;
+
+        beforeEach(function() {
+            videoDeferred = Q.defer();
+
+            XrayStub = function() {
+                return function(url, tagSelector) {
+                    return function(callback) {
+                        callback(null, 'http://videocdn/video.mp4');
+                    };
+                };
+            };
+
+            xboxClips.__set__('Xray', XrayStub);
+
+            sinon.stub(videos, 'upload_from_url', function(url, fn, id, duration) {
+                return videoDeferred.promise;
+            });
+
+            videoDeferred.resolve(1234);
+        });
+
+        afterEach(function() {
+            videos.upload_from_url.restore();
+        });
+
+        it('reads page for video source and uploads', function(done) {
+            var url = 'http://www.xboxdvr.com/PUBA/clip1234';
+            xboxClips.upload_xboxdvr_video(url).then(function(msg) {
+                try {
+                    expect(msg.pictureId).to.equal(1234);
+                    sinon.assert.calledWith(videos.upload_from_url,
+                        'http://videocdn/video.mp4',
+                        '1234.mp4',
+                        '1234', 28, 18);
+
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            });
+        });
+    });
+});
