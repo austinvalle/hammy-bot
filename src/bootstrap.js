6193d2ce5d50bd9ba278b02bd6f8d5c0dbeaf427
moosebot <austinvalle@gmail.com>
2018-06-14 06:39:00 -0400

    refactored bootstrap to use async/await

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,7 +6,7 @@ const initialize_client = () => {
 };
 
 const register_events = () => {
-	client.chat.on('message', (ev, msg) => {
+	client.chat.on('message', async (ev, msg) => {
 		for (let i = 0; i < message_events.length; i++) {
 			const message_event = message_events[i];
 			const matches = msg.match(message_event.regex);
@@ -15,19 +15,14 @@ const register_events = () => {
 				client.start_typing(ev);
 				if (message_event.allow_multiple) {
 					for (let m = 0; m < matches.length; m++) {
-						message_event.callback(matches[m]).then((msg) => {
-							return client.send_message(ev.conversation_id.id, msg.segments, msg.pictureId);
-						}).then(() => {
-							client.stop_typing(ev);
-						});
+						const msg = await message_event.callback(matches[m]);
+						await client.send_message(ev.conversation_id.id, msg.segments, msg.pictureId);
 					}
 				} else {
-					message_event.callback(matches[0]).then((msg) => {
-						return client.send_message(ev.conversation_id.id, msg.segments, msg.pictureId);
-					}).then(() => {
-						client.stop_typing(ev);
-					});
+					const msg = await message_event.callback(matches[0]);
+					await client.send_message(ev.conversation_id.id, msg.segments, msg.pictureId);
 				}
+				client.stop_typing(ev);
 			}
 		}
 	});
